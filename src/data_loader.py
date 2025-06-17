21c895e3f4b617e09891616d1a0468ae7e6849b6
kumsa Mergia <kumsamergia@gmail.com>
2025-06-17 13:12:30 +0300

    Refactor DataLoader class to support additional preprocessing steps

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,15 +1,9 @@
 import pandas as pd
 
 class DataLoader:
-    def __init__(self, filepath, delimiter='\t'):
+    def __init__(self, filepath, delimiter='|'):
         self.filepath = filepath
         self.delimiter = delimiter
-        self.df = None
 
     def load(self):
-        try:
-            self.df = pd.read_csv(self.filepath, delimiter=self.delimiter, low_memory=False)
-            return self.df
-        except Exception as e:
-            print(f"Error loading file: {e}")
-            return None
+        return pd.read_csv(self.filepath, delimiter=self.delimiter, low_memory=False)
